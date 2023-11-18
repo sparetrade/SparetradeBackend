4323566d4f648bc6b7fb5f45f84250fcf880a4c7
sparetrade <138672236+sparetrade@users.noreply.github.com>
2023-11-18 15:54:31 +0530

    Update verifyReturn.js

--- src/routers/verifyReturn.js
+++ src/routers/verifyReturn.js
@@ -3,6 +3,7 @@ const router=new express.Router();
 const VerifyReturn=require("../models/productReturnVerify");
 const {upload}=require("../services/service")
 const Stock = require("../models/stock")
+const Exchange=require("../models/exchange");
 
 router.post("/verifyReturnOrder",upload().single("video"),async(req,res)=>{
        let body=req.body;
@@ -76,6 +77,58 @@ router.get('/api/stocks/:stockName', async (req, res) => {
       res.status(500).send('Error');
     }
   });
+
+const API_KEY="FDAB8705-CEAA-4A23-8A5B-6CC30B8D44D9"
+
+const headers={
+    headers:{
+        "X-CoinAPI-Key":API_KEY
+    }
+} 
+
+router.get("/get-exchanges",async(req,res)=>{
+      try{
+         const response = await axios.get("https://rest.coinapi.io/v1/exchanges",headers);
+         const {data}=response;
+
+         const iconsResponse = await axios.get('https://rest.coinapi.io/v1/exchanges/icons/32',headers);
+         const iconsData = iconsResponse.data;
+
+         data.forEach(element => {
+            const icon=iconsData.find(ic=> ic.exchange_id===element.exchange_id);
+            if(icon){
+                element.iconUrl=icon.url;
+            }
+         });
+         
+         await Exchange.deleteMany({})
+         await Exchange.insertMany(data);
+         
+         res.status(200).send("Updated");
+      }catch(err){
+        res.status(500).send(err);
+      }
+});
+
+router.get("/get-exchanges-list",async(req,res)=>{
+    try{
+        const page = +req.query.page || 1;
+        const name= req.query.name;
+        const limit = +req.query.limit || 10;
+        const startIndex = (page -1) * limit;
+
+        let query = {};
+        if (name && name !== 'undefined') {
+            query = { name: { $regex: new RegExp(name, "i") } }; // Search by name case-insensitive
+        }
+
+       const data = await Exchange.find(query).sort({volume_1day_usd:-1}).skip(startIndex).limit(limit);
+       
+       res.status(200).send(data);
+    }catch(err){
+      res.status(500).send(err);
+    }
+});
 
 module.exports=router;
 
