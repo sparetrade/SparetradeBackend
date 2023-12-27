77700594fb9fb28388414df768277443e6264624
rajeshtiwari2503 <rajeshtiwari2503@gmail.com>
2023-12-27 12:58:54 +0530

    fdS

--- src/routers/payment.js
+++ src/routers/payment.js
@@ -146,6 +146,37 @@ router.post("/paymentVerification",async(req,res)=>{
      }
 });
 
+router.post("/walletPayment",async(req,res)=>{
+  try{
+   const pay=await instance.orders.create({
+     amount: (+req.body.amount)*100,
+     currency: "INR",
+   });
+   res.send(pay);
+  }catch(err){
+       res.status(400).send(err);
+  }
+});
+
+router.post("/paymentVerificationForWallet",async(req,res)=>{
+  const {razorpay_order_id,razorpay_payment_id,razorpay_signature,amount,_id}=req.body.response;
+  const body=razorpay_order_id + "|" + razorpay_payment_id;
+  const expectedSignature=crypto.createHmac('sha256',process.env.RAZORPAY_KEY_SECRET).update(body.toString()).digest("hex");
+  const isAuthentic=expectedSignature===razorpay_signature;
+  if(isAuthentic){
+    try{
+     let brand=await BrandModel.findById(_id);
+     brand.wallet += +amount;
+     await brand.save();
+     res.json({status:true,msg:"Added to wallet"});
+    }catch(err){
+      res.status(400).send(err);
+    }
+  }else{
+    res.status(401).send("Not Authorized");
+  }
+});
+
 router.post("/brandDuePayment",async(req,res)=>{
       try{
         //  let paymentDetail= {
